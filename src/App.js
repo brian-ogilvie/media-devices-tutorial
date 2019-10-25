b98ba317a059869466fb093c2eb322ad194babc1
Brian Ogilvie <brian.ogilvie@story2.com>
2019-10-25 02:48:49 -0500

    remove part 1 code

--- src/App.js
+++ src/App.js
@@ -4,27 +4,7 @@ import VideosList from './components/VideosList';
 import OrientationWarning from './components/OrientationWarning';
 
 function App() {
-  const [stream, setStream] = useState(null);
   const [videos, setVideos] = useState([]);
-
-  const getStream = async () => {
-    const mediaStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true,
-    });
-    setStream(mediaStream);
-  };
-
-  const releaseStream = () => {
-    if (stream !== null) {
-      stream.getTracks().forEach(track => track.stop());
-    }
-  };
-
-  useEffect(() => {
-    getStream();
-    return releaseStream;
-  }, []);
 
   return (
     <div className="App">
